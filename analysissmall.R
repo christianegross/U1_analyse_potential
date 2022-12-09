eca02b910a8b89a47c2f877f60a64f1ca317c441
Christiane Gross <chgross.wickede@gmail.com>
2022-12-09 17:52:48 +0100

    added explaining comments to analysissmall, cleaned up old code

--- analysissmall.R
+++ analysissmall.R
@@ -12,17 +12,17 @@ option_list <- list(
     make_option(c("-s", "--bootsamples"), type = "integer", default = 500,
     help = "how many bootstrapsamples should be drawn [default %default]"),
     make_option(c("-r", "--Ns"), type = "integer", default = 3,
-    help = "Ns of lattice [default %default]"),
+    help = "L of lattice [default %default]"),
 
     make_option(c("-t", "--Nt"), type = "integer", default = 3,
-    help = "Nt of lattice [default %default]"),
+    help = "T of lattice [default %default]"),
     make_option(c("-n", "--nape"), type = "integer", default = 0,
     help = "Number of APE-smears that were done [default %default]"),
 
     make_option(c("-j", "--job"), type = "integer", default = 0,
     help = "qbig job number [default %default]"),
     make_option(c("--t1"), type = "integer", default = 0,
-    help = "boundaries for determining meff [default %default]"),
+    help = "boundaries for determining meff if no table is given [default %default]"),
 
     make_option(c("-a", "--alpha"), type = "double", default = 1.0,
     help = "alpha used in APE-smearing [default %default]"),
@@ -32,21 +32,19 @@ option_list <- list(
     make_option(c("--fraction"), type = "double", default = 1,
     help = "fraction of lattice that was measured [default %default]"),
     make_option(c("-x", "--xi"), type = "double", default = 1,
-    help = "xi used in lattice [default %default]"),
+    help = "xi used in lattice, only used if xidiff = TRUE [default %default]"),
 
     make_option(c("--analyse"), action = "store_true", default = FALSE,
     help = "if true, potential values are determined with
             effective mass [default %default]"),
     make_option(c("--plaquette"), action = "store_true", default = FALSE,
     help = "if true, plaquette and anisotropy are determined [default %default]"),
-    make_option(c("--potential"), action = "store_true", default = FALSE,
-    help = "if true, potential is determined [default %default]"),
 
     make_option(c("--smearing"), action = "store_true", default = FALSE,
     help = "are the calculations done based on
             smeared lattices? (changes filenames) [default %default]"),
     make_option(c("--xidiff"), action = "store_true", default = FALSE,
-    help = "Is xi different to Ns / Nt? [default %default]"),
+    help = "Is xi different to L / T? [default %default]"),
     make_option(c("--nsave"), type = "integer", default = 100,
     help = "Step between saved configurations [default %default]"),
 
@@ -65,6 +63,7 @@ opt <- args$options
 }
 
 if (TRUE) {
+# set constants
 source(opt$myfunctions)
 beta <- opt$beta
 skip <- opt$skip
@@ -83,8 +82,12 @@ nape <- opt$nape
 alpha <- opt$alpha
 }
 
-
+# analyse: determine potential points vie bootstrapping Wilson
+# loops, determining effective masses and fitting to that
 if (opt$analyse) {
+
+# either read in existing table or generate new dataframe
+# for boundaries of the plateaus of the effective masses
 namelistbounds <- sprintf(
             "%stableanalysissmallNs%dNt%dbeta%fxi%f.csv",
             opt$plotpath, Ns, Nt, beta, xi)
@@ -108,6 +111,8 @@ if (file.exists(namelistbounds)) {
 
 
 print(listbounds)
+
+# open pdf for plots, open lists for results
 filenameforplots <- sprintf(
             "%splots2p1dsmallNs%dNt%dbeta%fxi%fbs%d.pdf",
             opt$plotpath, Ns, Nt, beta, xi, bootsamples)
@@ -121,6 +126,10 @@ listfits <- list(FALSE, FALSE, FALSE, FALSE, FALSE, FALSE)
 listtauint <- list()
 listpotshort <- data.frame(R = NA, m = NA, dm = NA)
 
+# iterate over every possible combination of (x, y)
+# determine the expectation values, the effective masses
+# set the plateau, plot the loops and the effective masses.
+# save everything in lists
 for (x in seq(0, 3)) {
     for (y in seq(0, 3)) {
     if (x > 0 || y > 0) {
@@ -132,10 +141,6 @@ for (x in seq(0, 3)) {
                 "%sresult2p1d.u1potential.Nt%d.Ns%d.b%f.xi%f.nape%d.alpha%fnonplanar",
                 opt$respath, Nt, Ns, beta, xi, nape, alpha)
 
-
-        #read in and determine Wilson-Loops
-    #~     message("try to read ", filename)
-    #~     if (!wlexist) {
         if (y == 0 || x == 0) {
             start <- 1
             title <- sprintf(
@@ -152,21 +157,15 @@ for (x in seq(0, 3)) {
                 (length(WL$cf[, 1]) + skip - 1) * opt$nsave, skip * opt$nsave)
 
         uwerrresults <- uwerr.cf(WL)
-#~         print(uwerrresults$uwcf)
-#~         message("uwerr calculated")
 
-        # determine effective masses, fit one effective mass
-        # therefore set bordes. borders may have to be adjusted for anisotropic lattices
+        # get boundaries for masses from table
         t1 <- listbounds$lower[listbounds$y == y & listbounds$x == x]
         t2 <- listbounds$upper[listbounds$y == y & listbounds$x == x]
         resulteffmass <- deteffmasssmall(WL, x, y, t1, t2)
-#~         newline <- resulteffmass[[1]]
-#~         ylim <- resulteffmass[[2]]
-#~         listresults <- resulteffmass[[3]]
 
-
-        # plot results, save results off meff
-        # in short and long form (with and without bootstrapsamples
+        # plot, with additional lines indicating half the lattice
+        # extent, save results of meff
+        # in short and long form (with and without bootstrapsamples)
         names(resulteffmass[[3]]) <- c("effmass", "x", "y")
 
         try(plot(resulteffmass[[3]][[1]], xlab = "t / a_t", ylab = "Meff",
@@ -182,11 +181,11 @@ for (x in seq(0, 3)) {
         arrows((Nt - start - 1) / 2, 10, (Nt - start - 1) / 2, -10,
                 angle = 90, length = 0.1, code = 0, col = 2)
         drawallticks(all = FALSE, inward = TRUE)
-#~         message("plots done")
-#~         try(plot(WL.effmass, xlab = "t / a_t", ylab = "Meff", main = sprintf("%s, t1 = %d, t2 = %d, p = %f", title, t1, t2, WL.effmass$effmassfit$Qval), ylim = c(0,1)))
         listfits[[4 * x + y]] <- resulteffmass[[3]]
         listtauint[[4 * x + y]] <- uwerrresults
         listpotshort <- rbind(listpotshort, resulteffmass[[1]])
+
+        # plot autocorrelation times of non-bootstrapped results
         try(plot(x = uwerrresults$uwcf$t - 1, y = uwerrresults$uwcf$tauint,
                 main = sprintf("%s, autocorellation times", title),
                 xlab = "t", ylab = "tauint"))
@@ -221,8 +220,10 @@ write.table(listpotshort, file = filenamepot, row.names = FALSE)
 }
 
 
-
+# plaquette: determine the slope of the potential, r_0,
+# display the potential graphically, ...
 if (opt$plaquette) {
+    #read in files
     filenameforplots <- sprintf(
             "%spotential2p1dsmallplaqNs%dNt%dbeta%fxi%fbs%d.pdf",
             opt$plotpath, Ns, Nt, beta, xi, opt$bootsamples)
@@ -254,21 +255,14 @@ if (opt$plaquette) {
     bootsamples <- length(listmeff[[5]][[1]]$massfit.tsboot[, 1])
 
     if (TRUE) { #plaquette
+        # determine the plaquette with uwerr
+        # (takes autocorrelation into account)
+        # anf with bootstrap
         plaquettedata <- readloopfilecfplaquette(file = filename,
                         skip = skip, Nsmax = Nsmax, Ntmax = Ntmax)
         plaquettedata <- bootstrap.cf(plaquettedata, boot.R = bootsamples)
         plaquette <- plaquettedata$cf0[1]
         dp <- plaquettedata$tsboot.se[1]
-#~         plaquette <- listmeff[[5]][[1]]$cf0[1]
-#~         dp <- listmeff[[5]][[1]]$tsboot.se[1]
-#~         print("cf0")
-#~         print(listmeff[[5]][[1]]$cf0)
-#~         print("se")
-#~         print(listmeff[[5]][[1]]$se)
-#~         print(names(listmeff[[5]][[1]]))
-#~         print(listmeff[[5]][[2]])
-#~         print(listmeff[[5]][[3]])
-
         measurements <- read.table(filename, header = FALSE, skip = opt$skip)
         plaquettedatauwerr <- uwerrprimary(measurements[, Nsmax + 3])
         plaquetteuwerr <- plaquettedatauwerr$value
@@ -276,6 +270,12 @@ if (opt$plaquette) {
     }
 
     if (TRUE) { # slope, ratio
+
+    # Read in V(x=1, y=0) and V(sqrt(2)), then determine ratio and
+    # slope of these data. Try to determine slope with a linear fit,
+    # else determine slope for each bootstrapsample.
+    # If no points are found, results are set to NA
+
 
     fnlin <- function (par, x, boot.r, ...) par[1]  + par[2]  *  x
 
@@ -323,7 +323,6 @@ if (opt$plaquette) {
             icslope <- mean(bsicslope)
             dicslope <- sd(bsicslope)
             chislope <- NA
-
         }
     } else {
         print("no points for xi found")
@@ -341,6 +340,7 @@ if (opt$plaquette) {
     }
     }
 
+    # plot slope either as the fit or as the two points
     title <- sprintf("beta = %f, xi_in = %f, Ns = %d, Nt = %d",
                 beta, xi, Ns, Nt)
     error3 <- try(plot(fit.result, xlab = "r / a_s",
@@ -352,6 +352,7 @@ if (opt$plaquette) {
     }
 
     if (TRUE) { #potential +string tension, r0
+    # read in all points of the potential
         fnpot <- function (par, x, boot.r, ...) par[1]  + par[2]  *  x  + par[3]  *  log(x)
         fnforce <- function (par, x, boot.r, ...) (- 1.0)  *  par[2]  *  x^2 - par[3]  *  x
 
@@ -380,7 +381,10 @@ if (opt$plaquette) {
         print(x)
         print(y)
 
-
+        # try to find a fit to the expected form,
+        # from that determine string tension, rzero,
+        # the offset of the potential and the factor in front of the logarithm
+        # if that fails, set results to NA
         fit.pot <- try(bootstrap.nlsfit(fnpot, c(1, 1, 1),
                 y = V, x = sqrt(x^2 + y^2), bsamples, mask = mask))
         if (!inherits(fit.pot, "try-error")) {
@@ -431,7 +435,9 @@ if (opt$plaquette) {
         }
     }
 
-    if (TRUE) { #calculate ratio with subtracted intercepts
+    if (TRUE) {
+        # calculate ratio, but subtract either
+        # the intercept of the slope or the additive constant
         bsratioslope <- c()
         bsratiopot <- c()
         for (bs in seq(1, bootsamples)) {
@@ -443,6 +449,10 @@ if (opt$plaquette) {
         ratiopot <- mean(bsratiopot)
         dratiopot <- sd(bsratiopot)
     }
+
+    # Make a list of all results plus bootstrapsamples,
+    # a data frame of the results,
+    # and save them. Only print columnnames if the file is empty.
     resultssummary <- list(slope = slope, dslope = dslope, bsslope = bsslope, chislope = chislope,
         ratio = ratio, dratio = dratio, bsratio = ratiobootsamples,
         st = st, dst = dst, bsst = bsst,
@@ -476,7 +486,6 @@ if (opt$plaquette) {
      #job SLURM jobID
      #boot number of b ootsamples
 
-#~     print(results)
     for (i in seq(1, length(crz))) {
 #~         message(i)
 #~ message(opt$beta, " a ", xi, " a ", opt$Nt, " a ", opt$Ns, " a ", plaquette,
@@ -494,9 +503,7 @@ if (opt$plaquette) {
          rzero = rzeros[i], drzero = drzeros[i], c = crz[i],
          puw = plaquetteuwerr, dpuw = dpuwerr, job = opt$job, boot = bootsamples,
          nom = length(measurements[, 1]) + opt$skip, skip = opt$skip)
-#~         print(newline)
         results <- rbind(results, newline)
-#~         print(results)
     }
     results <- results[-1, ]
     print(results)
@@ -512,6 +519,9 @@ if (opt$plaquette) {
             opt$plotpath, Ns, Nt, beta, xi, bootsamples)
     saveRDS(resultssummary, file = nameresults)
 
+    # read in results of the temporal normal potential,
+    # to compare ineger and non-integer distances,
+    # plot both, once zoomed out and once zoomed in
     filenamefineinteger <- sprintf(
             "%sfitresultfinesubtractedb%fxi%fNs%dbsamples%domit0l0.RData",
             opt$plotpath, beta, xi, Ns, bootsamples)
@@ -520,9 +530,8 @@ if (opt$plaquette) {
             "%sfitresultfinesubtractedb%fxi%fNs%dnape%dalpha%fbsamples%domit0l0.RData",
             opt$plotpath, beta, xi, Ns, nape, alpha, bootsamples)
     }
-#~     print(filenamefineinteger)
     resultfineinteger <- try(readRDS(filenamefineinteger))
-#~     print(resultfineinteger)
+    # zoomed out
     try(plot(fit.pot, ylab = "a_tV(r)", xlab = "r / a_s",
             main = "Potential including nonplanar points"))
     try(errorpolygon(X = seq(0, 7, by = 0.05),
@@ -530,27 +539,10 @@ if (opt$plaquette) {
     try(legend(x = "topleft", legend = c("non-integer", "integer"),
             col = c(1, 2), pch = c(1, 2)))
 
+    # zoomed in
     try(plot(resultfineinteger, ylab = "a_tV(r)", xlab = "r / a_s",
             main = "Potential including nonplanar points", col = 2, pch = 2))
     try(errorpolygon(X = seq(0, 8, by = 0.05), fit.pot, col.p = 1, pch = 1))
     try(legend(x = "topleft", legend = c("non-integer", "integer"),
             col = c(1, 2), pch = c(1, 2)))
-
-
 }
-
-#~     WL <- readloopfilecfanisotropy(file = filename, skip = skip, Nsmax = Nsmax, Ntmax = Ntmax)
-#~     WL <- bootstrap.cf(WL, boot.R = bootsamples)
-#~     print(names(WL))
-#~     print(names(WL$cf.tsboot))
-#~     print(WL)
-#~     print(WL$cf.tsboot)
-#~     print(WL$cf.tsboot$t)
-#~     WL.effmass <- bootstrap.effectivemass(WL, type = "log")
-#~     print(names(WL.effmass))
-#~     plaquette <- WL$cf0[2]
-#~     dp <- WL$tsboot.se[2]
-#~     xic <- WL.effmass$t0[1]
-#~     dxic <- WL.effmass$se[1]
-#~     xic2 <- WL$cf0[2] / WL$cf0[1]
-#~     dxic2 <- sqrt((WL$tsboot.se[2] / WL$cf0[1])^2 +(WL$cf0[2] * WL$tsboot.se[1] / WL$cf0[1]^2)^2)
