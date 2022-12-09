ab0b6a0fe51ceb4ba68f4f60cbc202d60a83ac40
Christiane Gross <chgross.wickede@gmail.com>
2022-12-09 15:32:37 +0100

    style changes for more files

--- converttablelatex.R
+++ converttablelatex.R
@@ -1,7 +1,11 @@
-args <- commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly = TRUE)
 name <- as.character(args[1])
 namenew <- as.character(args[2])
 
-data <- read.table(name, sep=",", header=TRUE)
+data <- read.table(name, sep = ",", header = TRUE)
 print(data)
-write.table(data, namenew, sep='$ & $', eol='$\\\\\n$', col.names=TRUE, row.names=FALSE)
+write.table(data, namenew, sep = "$ & $",
+        eol = "$\\\\\n$", col.names = TRUE, row.names = FALSE)
+string <- sprintf("the table %s has been copied to %s with latex style",
+                    name, namenew)
+print(string)
