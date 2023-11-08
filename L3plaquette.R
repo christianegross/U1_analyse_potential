418a218cea076f9885314d758c5e6f946f155233
Christiane Gross <chgross.wickede@gmail.com>
2023-11-08 13:24:09 +0100

    L3: corrected errors with input file, include unbiased results in an easy location.

--- L3plaquette.R
+++ L3plaquette.R
@@ -23,6 +23,7 @@ option_list <- list(
 )
 
 ## the inputfile should be formatted the same as for chosepredict
+## datapath is given separately, the datapath from the inputfile is ignored
 
 parser <- OptionParser(usage = "%prog [options]", option_list = option_list)
 args <- parse_args(parser, positional_arguments = 0)
@@ -30,21 +31,22 @@ opt <- args$options
 source(paste(opt$myfunctions, "myfunctions.R", sep = ""))
 }
 
-input <- read.table(opt$input, header = TRUE)
+input <- read.table(opt$input, header = TRUE, sep=",")
 input <- input[opt$line, ]
+print(input)
 
 githash <- printgitcommit(opt$myfunctions)
 bootsamples <-  500
 
 
 xiconststr <- ""
-if (opt$xiconst) xiconststr <- "xiconst"
-print(opt$singlemulti)
-if (opt$singlemulti == "single") endname <- sprintf("%sbeta%fomit%d%sllxi%dllr0%d", type, opt$beta, opt$omit, xiconststr, opt$lowlimxi, opt$lowlimpot)
-if (opt$singlemulti == "multi") endname <- sprintf("multi%sbeta%fomit%d%slowlim%d", type, opt$beta, opt$omit, xiconststr, opt$lowlimxi)
-if (opt$crzero != -1.65) endname <- sprintf("%sc%.2f", endname, opt$crzero)
-if (opt$aic) endname <- sprintf("%saic", endname)
-if (opt$scaletauint) endname <- sprintf("%sscaletauintetp%d", endname, opt$errortotpot)
+if (input$xiconst) xiconststr <- "xiconst"
+print(input$singlemulti)
+if (input$singlemulti == "single") endname <- sprintf("%sbeta%fomit%d%sllxi%dllr0%d", input$type, input$beta, input$omit, xiconststr, input$lowlimxi, input$lowlimpot)
+if (input$singlemulti == "multi") endname <- sprintf("multi%sbeta%fomit%d%slowlim%d", input$type, input$beta, input$omit, xiconststr, input$lowlimxi)
+if (input$crzero != -1.65) endname <- sprintf("%sc%.2f", endname, input$crzero)
+if (input$aic) endname <- sprintf("%saic", endname)
+if (input$scaletauint) endname <- sprintf("%sscaletauintetp%d", endname, input$errortotpot)
 
 
 S <-  opt$sparam
@@ -53,19 +55,20 @@ therm16 <-  c(500, 500, 500, 500, 500, 1000, 2000)
 therm3 <-  c(1333, 1333, 1333, 1333, 2666, 5333, 10666)
 
 # xiconststr <- ""
-# if (opt$xiconst) xiconststr <- "xiconst"
-# print(opt$singlemulti)
-# if (opt$singlemulti = =  "single") endname <- sprintf("%sbeta%fomit%d%slowlim%d", opt$type, opt$beta, opt$omit, xiconststr, opt$lowlimfitpot)
-# if (opt$singlemulti = =  "multi") endname <- sprintf("multi%sbeta%fomit%d%slowlim%d", opt$type, opt$beta, opt$omit, xiconststr, opt$lowlimfitpot)
-# if (opt$aic) endname <- sprintf("%saic", endname)
-# if (opt$scaletauint) endname <- sprintf("%sscaletauintetp%d", endname, opt$errortotpot)
+# if (input$xiconst) xiconststr <- "xiconst"
+# print(input$singlemulti)
+# if (input$singlemulti = =  "single") endname <- sprintf("%sbeta%fomit%d%slowlim%d", input$type, input$beta, input$omit, xiconststr, input$lowlimfitpot)
+# if (input$singlemulti = =  "multi") endname <- sprintf("multi%sbeta%fomit%d%slowlim%d", input$type, input$beta, input$omit, xiconststr, input$lowlimfitpot)
+# if (input$aic) endname <- sprintf("%saic", endname)
+# if (input$scaletauint) endname <- sprintf("%sscaletauintetp%d", endname, input$errortotpot)
 
 
 ## Read in result for L = 3
 
 
 list3 <- list()
-P3 <- array(NA, dim = c(opt$bootsamples, length(Nt)))
+P3 <- array(NA, dim = c(input$bootsamples, length(Nt)))
+Punbiased <- c()
 
 
 pdf(sprintf("L3%s.pdf", endname), title = "")
@@ -73,6 +76,7 @@ pdf(sprintf("L3%s.pdf", endname), title = "")
 for (i in seq(1, 7)) {
     print(i)
     betaname <- paste0("xi", i, "beta")
+    if (i==1) betaname <- "beta"
     beta <- input[betaname]
     filengthame3 <-  sprintf("%s/result2p1d.u1potential.Nt%d.Ns3.b%f.xi%f.nape0.alpha1.000000nonplanar", opt$datapath, Nt[i], beta, 16 / Nt[i])
     data3 <-  readloopfilecfonecolumn(file = filengthame3, path = "", skip = therm3[i], column = 6, memsafe = TRUE)
@@ -82,20 +86,23 @@ for (i in seq(1, 7)) {
     plot(uwerr3, main = paste("L = 3, T = ", Nt[i]))
     bootl <- ceiling(4 * uwerr3$tauint^2)
     plaquettecf <- bootstrap.cf(data3,
-                boot.R = opt$bootsamples, boot.l = bootl)
+                boot.R = input$bootsamples, boot.l = bootl)
     list3[[i]] <-  list(uwerr = uwerr3, cf = plaquettecf, beta = beta)
     P3[, i] <- plaquettecf$cf.tsboot$t[, 1]
+    Punbiased[i] <- uwerr3$value
 
 }
 
 names(list3) <- c("xi1", "xi2", "xi3", "xi4", "xi5", "xi6", "xi7")
 list3[["plaquette"]] <- P3
+list3$Punbiased <- Punbiased
+list3$githash <- githash
 
 saveRDS(object = list3, file = sprintf("plaquetteL3%s.RData", endname))
 
 ## check for normal distribution
 
-for (i in seq(1, length(betas))) {
+for (i in seq(1, 7)) {
     shapiro3 <- shapiro.test(x = P3[, i])
     print(shapiro3)
     qqnorm(y = P3[, i], main = paste("L = 3, xi", i, shapiro3$p.value))
