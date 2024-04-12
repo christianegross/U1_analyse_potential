6cc96d649538e514694df9762ab49ddaaf6c52ff
Christiane Gross <chgross.wickede@gmail.com>
2024-04-12 10:02:05 +0200

    correct nalist

--- chosepredict.R
+++ chosepredict.R
@@ -400,7 +400,7 @@ for (fun in c(fnlin, fnpar, fncub, fnqar, fnqin)) {
 # print(result)
 
 listfits$githash <- githash
-listfits$nalist <- nalist
+listfits$nalist <- resultslist$nalist
 
 namepol <- sprintf("%s/polynomialbetachosen%scont%d.csv", opt$respath, endnamewrite, opt$indexfitcontlim)
 write.table(resultspolynomial, namepol, col.names = TRUE, row.names = FALSE)
