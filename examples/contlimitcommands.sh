5f36f3c6613c5266408032985411e8b3a3a0ef0f
Christiane Gross <chgross.wickede@gmail.com>
2025-07-31 18:13:37 +0200

    some example commands not generated by scripts

---
+++ examples/contlimitcommands.sh
@@ -0,0 +1,66 @@
+Rscript predictbeta.R --xiconst --aic --scaletauint --type normal   --summaryname resultsummary2p1dnormalb1.700Ns16.csv   --beta 1.65 --omit 1 --lowlimxi 1 --lowlimpot 0 --errortotpot
+Rscript predictbeta.R --xiconst --aic --scaletauint --type normal   --summaryname resultsummary2p1dnormalb1.700Ns16.csv   --beta 1.70 --omit 1 --lowlimxi 1 --lowlimpot 0 --errortotpot
+Rscript predictbeta.R --xiconst --aic --scaletauint --type sideways --summaryname resultsummary2p1dsidewaysb1.700Ns16.csv --beta 1.65 --omit 1 --lowlimxi 1 --lowlimpot 0 --errortotpot
+Rscript predictbeta.R --xiconst --aic --scaletauint --type sideways --summaryname resultsummary2p1dsidewaysb1.700Ns16.csv --beta 1.70 --omit 1 --lowlimxi 1 --lowlimpot 0 --errortotpot
+Rscript predictbeta.R --xiconst --aic --scaletauint --type normal   --summaryname resultsummary2p1dnormalb1.700Ns16.csv   --beta 1.65 --omit 0 --lowlimxi 1 --lowlimpot 0 --errortotpot
+Rscript predictbeta.R --xiconst --aic --scaletauint --type normal   --summaryname resultsummary2p1dnormalb1.700Ns16.csv   --beta 1.70 --omit 0 --lowlimxi 1 --lowlimpot 0 --errortotpot
+Rscript predictbeta.R --xiconst --aic --scaletauint --type sideways --summaryname resultsummary2p1dsidewaysb1.700Ns16.csv --beta 1.65 --omit 0 --lowlimxi 1 --lowlimpot 0 --errortotpot
+Rscript predictbeta.R --xiconst --aic --scaletauint --type sideways --summaryname resultsummary2p1dsidewaysb1.700Ns16.csv --beta 1.70 --omit 0 --lowlimxi 1 --lowlimpot 0 --errortotpot
+Rscript predictbeta.R --xiconst --aic --scaletauint --type normal   --summaryname resultsummary2p1dnormalb1.700Ns16.csv   --beta 1.65 --omit 0 --lowlimxi 1 --lowlimpot 0
+Rscript predictbeta.R --xiconst --aic --scaletauint --type normal   --summaryname resultsummary2p1dnormalb1.700Ns16.csv   --beta 1.70 --omit 0 --lowlimxi 1 --lowlimpot 0
+Rscript predictbeta.R --xiconst --aic --scaletauint --type sideways --summaryname resultsummary2p1dsidewaysb1.700Ns16.csv --beta 1.65 --omit 0 --lowlimxi 1 --lowlimpot 0
+Rscript predictbeta.R --xiconst --aic --scaletauint --type sideways --summaryname resultsummary2p1dsidewaysb1.700Ns16.csv --beta 1.70 --omit 0 --lowlimxi 1 --lowlimpot 0
+Rscript predictbeta.R --xiconst --aic --scaletauint --type normal   --summaryname resultsummary2p1dnormalb1.700Ns16.csv   --beta 1.65 --omit 1 --lowlimxi 1 --lowlimpot 0
+Rscript predictbeta.R --xiconst --aic --scaletauint --type normal   --summaryname resultsummary2p1dnormalb1.700Ns16.csv   --beta 1.70 --omit 1 --lowlimxi 1 --lowlimpot 0
+Rscript predictbeta.R --xiconst --aic --scaletauint --type sideways --summaryname resultsummary2p1dsidewaysb1.700Ns16.csv --beta 1.65 --omit 1 --lowlimxi 1 --lowlimpot 0
+Rscript predictbeta.R --xiconst --aic --scaletauint --type sideways --summaryname resultsummary2p1dsidewaysb1.700Ns16.csv --beta 1.70 --omit 1 --lowlimxi 1 --lowlimpot 0
+
+Rscript chosepredict.R -i inputpredictcontlim.csv -l 1
+Rscript chosepredict.R -i inputpredictcontlim.csv -l 2
+Rscript chosepredict.R -i inputpredictcontlim.csv -l 3
+Rscript chosepredict.R -i inputpredictcontlim.csv -l 4
+Rscript chosepredict.R -i inputpredictcontlim.csv -l 5
+Rscript chosepredict.R -i inputpredictcontlim.csv -l 6
+Rscript chosepredict.R -i inputpredictcontlim.csv -l 7
+Rscript chosepredict.R -i inputpredictcontlim.csv -l 8
+Rscript chosepredict.R -i inputpredictcontlim.csv -l 9
+Rscript chosepredict.R -i inputpredictcontlim.csv -l 10
+Rscript chosepredict.R -i inputpredictcontlim.csv -l 11
+Rscript chosepredict.R -i inputpredictcontlim.csv -l 12
+Rscript chosepredict.R -i inputpredictcontlim.csv -l 13
+Rscript chosepredict.R -i inputpredictcontlim.csv -l 14
+Rscript chosepredict.R -i inputpredictcontlim.csv -l 15
+Rscript chosepredict.R -i inputpredictcontlim.csv -l 16
+
+ 
+Rscript L3plaquette.R -i inputpredictcontlim.csv -l 1
+Rscript L3plaquette.R -i inputpredictcontlim.csv -l 2
+Rscript L3plaquette.R -i inputpredictcontlim.csv -l 3
+Rscript L3plaquette.R -i inputpredictcontlim.csv -l 4
+Rscript L3plaquette.R -i inputpredictcontlim.csv -l 5
+Rscript L3plaquette.R -i inputpredictcontlim.csv -l 6
+Rscript L3plaquette.R -i inputpredictcontlim.csv -l 7
+Rscript L3plaquette.R -i inputpredictcontlim.csv -l 8
+Rscript L3plaquette.R -i inputpredictcontlim.csv -l 9
+Rscript L3plaquette.R -i inputpredictcontlim.csv -l 10
+Rscript L3plaquette.R -i inputpredictcontlim.csv -l 11
+Rscript L3plaquette.R -i inputpredictcontlim.csv -l 12
+Rscript L3plaquette.R -i inputpredictcontlim.csv -l 13
+Rscript L3plaquette.R -i inputpredictcontlim.csv -l 14
+Rscript L3plaquette.R -i inputpredictcontlim.csv -l 15
+Rscript L3plaquette.R -i inputpredictcontlim.csv -l 16
+
+
+
+Rscript ~/Documents/masterthesis/analyse_potential/average_contlimits.r --myfunction ~/Documents/masterthesis/analyse_potential/ -t plaq -m xi0.20v2
+Rscript ~/Documents/masterthesis/analyse_potential/average_contlimits.r --myfunction ~/Documents/masterthesis/analyse_potential/ -t beta -m xi0.20v2
+Rscript ~/Documents/masterthesis/analyse_potential/average_contlimits.r --myfunction ~/Documents/masterthesis/analyse_potential/ -t plaqinter -m xi0.20v2
+Rscript ~/Documents/masterthesis/analyse_potential/average_contlimits.r --myfunction ~/Documents/masterthesis/analyse_potential/ -t betainter -m xi0.20v2
+Rscript ~/Documents/masterthesis/analyse_potential/average_contlimits.r --myfunction ~/Documents/masterthesis/analyse_potential/ -t plaqsmall -m xi0.20v2
+Rscript ~/Documents/masterthesis/analyse_potential/average_contlimits.r --myfunction ~/Documents/masterthesis/analyse_potential/ -t ratio -m xi0.20v2
+Rscript ~/Documents/masterthesis/analyse_potential/average_contlimits.r --myfunction ~/Documents/masterthesis/analyse_potential/ -t plaq -m xi0.18v2
+Rscript ~/Documents/masterthesis/analyse_potential/average_contlimits.r --myfunction ~/Documents/masterthesis/analyse_potential/ -t beta -m xi0.18v2
+Rscript ~/Documents/masterthesis/analyse_potential/average_contlimits.r --myfunction ~/Documents/masterthesis/analyse_potential/ -t plaqinter -m xi0.18v2
+Rscript ~/Documents/masterthesis/analyse_potential/average_contlimits.r --myfunction ~/Documents/masterthesis/analyse_potential/ -t betainter -m xi0.18v2
+Rscript ~/Documents/masterthesis/analyse_potential/average_contlimits.r --myfunction ~/Documents/masterthesis/analyse_potential/ -t plaqsmall -m xi0.18v2
+Rscript ~/Documents/masterthesis/analyse_potential/average_contlimits.r --myfunction ~/Documents/masterthesis/analyse_potential/ -t ratio -m xi0.18v2
